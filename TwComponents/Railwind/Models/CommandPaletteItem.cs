e536561ced3105548b816b171e408a1908630936
Seth VanderZanden <sethvanderzanden28@gmail.com>
2024-11-01 12:37:43 -0400

    Search functionality and more

--- TwComponents/Railwind/Models/CommandPaletteItem.cs
+++ TwComponents/Railwind/Models/CommandPaletteItem.cs
@@ -3,6 +3,7 @@
 public partial class CommandPaletteItem<T>
 {
     public bool IsSelected { get; set; }
+    public bool IsSelectable { get; set; } = true;
     public string Title { get; set; } = string.Empty;
     public string Key { get; set; } = string.Empty;
     

0c531c2ed208f7a6a22464db74d9f36bd9de7b07
Seth VanderZanden <sethvanderzanden28@gmail.com>
2024-11-25 22:14:56 -0500

    support created

--- TwComponents/Railwind/Features/Toasters/ToasterService.cs
+++ TwComponents/Railwind/Features/Toasters/ToasterService.cs
@@ -1,4 +1,5 @@
 ﻿using Railwind.Common.Enums;
+using Railwind.Common.Models;
 
 namespace Railwind.Features.Toasters;
 
@@ -28,6 +29,40 @@ public class ToasterService
         {
             Id = Guid.NewGuid(),
             Message = "",
+            Title = title,
+            Duration = duration,
+            Severity = severity
+        };
+
+        OnToastCreate?.Invoke(toast);
+    }
+    
+    public void ShowOutcomeToast(Outcome outcome, int duration = 3000)
+    {
+        var title = outcome.Status switch
+        {
+            OutcomeStatus.Succeeded => "Success",
+            OutcomeStatus.Unauthorized => "Unauthorized",
+            OutcomeStatus.Canceled => "Cancelled",
+            OutcomeStatus.Processing => "Processing",
+            OutcomeStatus.Failed => "Error",
+            _ => ""
+        };
+        
+        var severity = outcome.Status switch
+        {
+            OutcomeStatus.Succeeded => ToastSeverity.Success,
+            OutcomeStatus.Unauthorized => ToastSeverity.Warning,
+            OutcomeStatus.Canceled => ToastSeverity.Info,
+            OutcomeStatus.Processing => ToastSeverity.Info,
+            OutcomeStatus.Failed => ToastSeverity.Error,
+            _ => ToastSeverity.Info
+        };
+
+        var toast = new Toast
+        {
+            Id = Guid.NewGuid(),
+            Message = outcome.Message,
             Title = title,
             Duration = duration,
             Severity = severity

22841d63d14816c4864834bb0bc9a42260cd286d
Seth VanderZanden <sethvanderzanden28@gmail.com>
2025-01-16 23:05:24 -0500

    progress

--- TwComponents/Railwind/Common/Models/Outcome.cs
+++ TwComponents/Railwind/Common/Models/Outcome.cs
@@ -6,7 +6,7 @@ public class Outcome
 {
     public OutcomeStatus Status { get; set; }
     public string Message { get; set; } = string.Empty;
-    
+
     public static Outcome Successful(string message = "") => new()
     {
         Message = message,
@@ -18,17 +18,16 @@ public class Outcome
         Message = message,
         Status = OutcomeStatus.Failed
     };
-    
+
     public static Outcome FromGenericOutcome<T>(Outcome<T> outcome) => new()
     {
         Message = outcome.Message,
         Status = outcome.Status
     };
-    
+
     public bool IsFailure => Status == OutcomeStatus.Failed;
     public bool IsSuccessful => Status == OutcomeStatus.Succeeded;
 }
-
 
 public class Outcome<T>
 {
@@ -42,36 +41,42 @@ public class Outcome<T>
         Message = message,
         Status = OutcomeStatus.Succeeded
     };
-    
-    public static Outcome<T> Failure(string message = "") => new ()
+
+    public static Outcome<T> Failure(string message = "") => new()
     {
         Data = default,
         Message = message,
         Status = OutcomeStatus.Failed
     };
-    
-      
-    public static Outcome<T> Invalid(string message = "") => new ()
+
+    public static Outcome<T> Standy() => new()
+    {
+        Data = default,
+        Message = string.Empty,
+        Status = OutcomeStatus.Failed
+    };
+
+    public static Outcome<T> Invalid(string message = "") => new()
     {
         Data = default,
         Message = message,
         Status = OutcomeStatus.Invalid
     };
-    
-    public static Outcome<T> Unauthorized(string message = "") => new ()
+
+    public static Outcome<T> Unauthorized(string message = "") => new()
     {
         Data = default,
         Message = message,
         Status = OutcomeStatus.Unauthorized
     };
-    
-    public static Outcome<T> Failure(T data = default, string message = "") => new ()
+
+    public static Outcome<T> Failure(T data = default, string message = "") => new()
     {
         Data = data,
         Message = message,
         Status = OutcomeStatus.Failed
     };
-    
+
     public bool IsFailure => Status == OutcomeStatus.Failed;
     public bool IsSuccessful => Status == OutcomeStatus.Succeeded;
 }

c5a9dade014e3601030a253098dfc045b29ee6b7
Seth VanderZanden <sethvanderzanden28@gmail.com>
2025-02-01 12:30:23 -0500

    correcting demo to .net 9

--- TwComponents/Demo/Program.cs
+++ TwComponents/Demo/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddRailwindServices();
+builder.Services.AddGeneralRailwindServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 

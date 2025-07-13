ade3b2c6b45b97327169002ea6afa415e13a365f
Seth <sethvanderzanden28@gmail.com>
2025-07-13 12:16:29 -0400

    changes

---
+++ TwComponents/Railwind/Common/Enums/TypographyType.cs
@@ -0,0 +1,12 @@
+﻿namespace Railwind.Common.Enums;
+
+public enum TypographyType
+{
+    H1Typography,
+    H2Typography,
+    H3Typography,
+    H4Typography,
+    H5Typography,
+    H6Typography,
+    PTypography,
+}
